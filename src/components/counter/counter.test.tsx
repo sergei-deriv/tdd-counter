bcb9865758e74d08f43debf51b84381e448bad51
Sergei Baranovski <sergei@deriv.com>
2023-03-09 18:00:26 +0300

    feat: add test for counter

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -4,9 +4,15 @@ import Counter from './counter';
 it('there is testId title', () => {
   render(<Counter />);
 
-  const title = screen.getByTestId(/counter title/i);
+  const title = screen.getByTestId(/title/i);
 
   expect(title).toBeInTheDocument();
 });
 
-// it('increase counter value', () => {});
+it('there is counter value', () => {
+  render(<Counter />);
+
+  const counter = screen.getByTestId(/counter/i);
+
+  expect(counter).toBeInTheDocument();
+});
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1 data-testid={'counter title'}>Counter</h1>
+      <h1 data-testid={'title'}>Counter</h1>
+      <h3 data-testid={'counter'}>{counter}</h3>
     </div>
   );
 };
