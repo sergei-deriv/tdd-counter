470bf4b35696723104107ce46323c6ae00ac8020
Sergei Baranovski <sergei@deriv.com>
2023-03-10 09:48:37 +0300

    feat: complete tests for inc/dec/reset buttons

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Counter from './counter';
 
 it('there is testId title', () => {
@@ -15,4 +16,59 @@ it('there is counter value', () => {
   const counter = screen.getByTestId(/counter/i);
 
   expect(counter).toBeInTheDocument();
+});
+
+it('there are buttons "+", "-", and "0"', () => {
+  render(<Counter />);
+
+  const increase_button = screen.getByText('+');
+  const decrease_button = screen.getByText('-');
+  const reset_button = screen.getByText('reset');
+
+  expect(increase_button).toBeInTheDocument();
+  expect(decrease_button).toBeInTheDocument();
+  expect(reset_button).toBeInTheDocument();
+});
+
+it('click "+" increases counter by 1', async () => {
+  render(<Counter />);
+
+  const increase_button = screen.getByText('+');
+  const counter = screen.getByTestId(/counter/i);
+
+  expect(counter.innerHTML).toBe('0');
+
+  await userEvent.click(increase_button);
+
+  expect(counter.innerHTML).toBe('1');
+});
+
+it('click "-" decreases counter by 1', async () => {
+  render(<Counter />);
+
+  const decrease_button = screen.getByText('-');
+  const counter = screen.getByTestId(/counter/i);
+
+  expect(counter.innerHTML).toBe('0');
+
+  await userEvent.click(decrease_button);
+
+  expect(counter.innerHTML).toBe('-1');
+});
+
+it('increase to 2 and then click "reset" set counter to 0', async () => {
+  render(<Counter />);
+
+  const increase_button = screen.getByText('+');
+  const reset_button = screen.getByText('reset');
+
+  const counter = screen.getByTestId(/counter/i);
+  expect(counter.innerHTML).toBe('0');
+
+  await userEvent.click(increase_button);
+  expect(counter.innerHTML).toBe('1');
+  await userEvent.click(increase_button);
+  expect(counter.innerHTML).toBe('2');
+  await userEvent.click(reset_button);
+  expect(counter.innerHTML).toBe('0');
 });
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -3,10 +3,19 @@ import { useState } from 'react';
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  const onClickHandler = (value: number) => {
+    setCounter(value);
+  };
+
   return (
     <div>
       <h1 data-testid={'title'}>Counter</h1>
       <h3 data-testid={'counter'}>{counter}</h3>
+      <div>
+        <button onClick={() => onClickHandler(counter + 1)}>+</button>
+        <button onClick={() => onClickHandler(counter - 1)}>-</button>
+        <button onClick={() => onClickHandler(0)}>reset</button>
+      </div>
     </div>
   );
 };
